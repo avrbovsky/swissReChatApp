104252ec9c709995351ff49bfdd308e29c00153b
Adrián Vrbovský <avrbovsky64@gmail.com>
2023-11-03 12:59:14 +0100

    feature/Add landing page

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [ChatComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDividerModule],
   exports: [ChatComponent],
 })
 export class ChatModule {}
